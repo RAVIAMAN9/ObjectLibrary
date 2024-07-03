b55ce3b2dbc5afc2d21b6165f176e4aaa9209c4f
amank <ravikumarjha25@gmail.com>
2024-07-03 10:23:37 +0530

    Added new Object library files

---
+++ src/main/java/com/ObjectLib/ObjectLibrary/Dto/Train.java
@@ -0,0 +1,51 @@
+package com.ObjectLib.ObjectLibrary.Dto;
+
+public class Train {
+
+    private Integer trainNumber;
+    private String trainName;
+    private String source;
+    private String destination;
+
+    public Integer getTrainNumber() {
+        return trainNumber;
+    }
+
+    public void setTrainNumber(Integer trainNumber) {
+        this.trainNumber = trainNumber;
+    }
+
+    public String getTrainName() {
+        return trainName;
+    }
+
+    public void setTrainName(String trainName) {
+        this.trainName = trainName;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Train{" +
+                "trainNumber=" + trainNumber +
+                ", trainName='" + trainName + '\'' +
+                ", source='" + source + '\'' +
+                ", destination='" + destination + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/ObjectLib/ObjectLibrary/ObjectLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.ObjectLib.ObjectLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjectLibraryApplication.class, args);
+	}
+
+}
